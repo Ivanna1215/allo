e3291ac6b5fbc722890ae56361570dc4fe51cd3d
Ivanna Kovaliv <ivanna.kovaliv1215@gmail.com>
2023-04-03 18:37:39 +0300

    updated main-page

--- cypress/support/page-objects/main-page.js
+++ cypress/support/page-objects/main-page.js
@@ -1,3 +1,5 @@
+import selectors from '../page-objects/selectors.js';
+
 class MainPage {
 
     visitURL(url) {
@@ -11,25 +13,25 @@ class MainPage {
     }
 
     selectMarketplace(marketplace) {
-        cy.wait(2000).get('[class="mh-catalog-btn"]').contains(marketplace).click();
+        cy.wait(2000).get(selectors.buttonSelector).contains(marketplace).click();
         return this;
     }
 
     selectCategory(category) {
-        cy.get('.mm__list').contains(category).trigger('mouseover').click({ force: true });
-        cy.get('.portal-category__list').should('be.visible');
+        cy.get(selectors.list).contains(category).trigger('mouseover').click({ force: true });
+        cy.get(selectors.portalCategoryList).should('be.visible');
         return this;
     }
 
     selectSubcategory(subcategory) {
-        cy.get('li[class="portal-category__item"] a[class="portal-category__item-link"]')
+        cy.get(selectors.portalCategoryItem)
             .contains(subcategory).click();
         return this;
     }
 
     filterByPriceRange(from, to) {
-        cy.get('.f-range__values .f-range__form-input:first').clear().type(from);
-        cy.get('.f-range__values .f-range__form-input:last').clear().type(to);
+        cy.get(selectors.formInputFirst).clear().type(from);
+        cy.get(selectors.formInputLast).clear().type(to);
         this.showResults();
         return this;
     }
@@ -49,7 +51,7 @@ class MainPage {
     }
 
     verifyPriceRangeFilter(from, to) {
-        cy.get('.v-pb__cur .sum').each((item) => {
+        cy.get(selectors.sum).each((item) => {
             const price = parseInt(item.text().replace(/[^0-9.-]+/g, ''));
             expect(price).to.be.within(from, to);
             return this;
@@ -79,14 +81,14 @@ class MainPage {
     }
 
     addItemsToTheBasket() {
-        cy.get('div[class="product-card"]').first().then(($card) => {
-            let name = $card.find('.product-card__title').text();
+        cy.get(selectors.productCard).first().then(($card) => {
+            let name = $card.find(selectors.productCardTitle).text();
             cy.wrap(name).as('ProductName');
 
-            cy.wrap($card).find('.v-btn--cart').trigger('mouseover').wait(1000).click();
+            cy.wrap($card).find(selectors.btnCart).trigger('mouseover').wait(1000).click();
         })
 
-        cy.get('.checkout_modal').then($modal => {
+        cy.get(selectors.checkoutModal).then($modal => {
             cy.get('@ProductName').then(name => {
                 cy.wrap($modal).should('contain', name);
             });
@@ -95,9 +97,9 @@ class MainPage {
     }
 
     closeCheckout() {
-        cy.get('.checkout_modal').then($modal => {
+        cy.get(selectors.checkoutModal).then($modal => {
             if ($modal.is(':visible')) {
-                cy.wrap($modal).find('[class="comeback"]').click();
+                cy.wrap($modal).find(selectors.comebacK).click();
             }
         })
         return this;
@@ -105,12 +107,12 @@ class MainPage {
 
     verifyTotalPrice() {
         let sum = 0;
-        cy.get('div[class="price-box__cur"]').each((price) => {
+        cy.get(selectors.priceBox).each((price) => {
             const text = price.text();
             const number = parseInt(text.replace(/[^0-9.-]+/g, ''));
             sum += number;
         }).then(() => {
-            cy.get('.total-box__price').then((total) => {
+            cy.get(selectors.totalBoxPrice).then((total) => {
                 const totalText = total.text();
                 const totalPrice = parseInt(totalText.replace(/[^0-9.-]+/g, ''));
                 expect(totalPrice).to.eq(sum);
@@ -120,48 +122,47 @@ class MainPage {
     }
 
     verifyDeleteButtonClickable() {
-        cy.get('.vi__close.remove').each(($el) => {
+        cy.get(selectors.closeRemove).each(($el) => {
             cy.wrap($el).first().click();
         })
         return this;
     }
 
     verifyBasketEmpty(text) {
-        cy.get('.cart-popup_empty').should('contain', text);
+        cy.get(selectors.cartPopupEmpty).should('contain', text);
         return this;
     }
 
     closeModal() {
-        cy.get('.v-modal__close-btn').click();
+        cy.get(selectors.closeBtn).click();
         return this;
     }
 
     searchItem(itemName) {
-        cy.get('#search-form__input').type(itemName);
-        cy.get('.search-form__submit-button').click();
+        cy.get(selectors.searchFormInput).type(itemName);
+        cy.get(selectors.searchFormSubmitButton).click();
         return this;
     }
 
     searchResults(searchItem) {
-        cy.get('.product-card').wait(1000).should('have.length.gt', 0);
-        cy.get('.product-card').each((item) => {
+        cy.get(selectors.productCard).wait(1000).should('have.length.gt', 0);
+        cy.get(selectors.productCard).each((item) => {
             cy.wrap(item)
-                .find('.product-card__title')
+                .find(selectors.productCardTitle)
                 .should('contain', searchItem);
         })
-
         return this;
     }
 
     signIn() {
-        cy.get('.mh-profile').click();
+        cy.get(selectors.profile).click();
         return this;
     }
 
     login(number,password) {
-        cy.get('#auth').type(number);
-        cy.get('#v-login-password').type(password);
-        cy.get('.modal-submit-button').click();
+        cy.get(selectors.auth).type(number);
+        cy.get(selectors.loginPassword).type(password);
+        cy.get(selectors.modalSubmitButton).click();
         return this;
     }
 
@@ -172,7 +173,7 @@ class MainPage {
     }
 
     showResults() {
-        cy.get('span.f-popup__btn-message').click().wait(2000);
+        cy.get(selectors.btnMessage).click().wait(2000);
         return this;
     }
 }
